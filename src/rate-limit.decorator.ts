f219619829e5f7ef29946b29fc2314fbb774022c
Dmytro Yanchuk <yanchukd98@gmail.com>
2023-06-21 12:26:54 +0300

    feat: add support for passing configs as a parameter to decorator

--- src/rate-limit.decorator.ts
+++ src/rate-limit.decorator.ts
@@ -1,4 +1,8 @@
 import { SetMetadata } from '@nestjs/common';
-import { RATE_LIMIT_SKIP } from './rate-limit.constants';
+import { RATE_LIMIT_CONFIG, RATE_LIMIT_SKIP } from './rate-limit.constants';
+import { RateLimitConfig } from './rate-limit.types';
+
+export const RateLimit = (config: Partial<RateLimitConfig>) =>
+  SetMetadata(RATE_LIMIT_CONFIG, config);
 
 export const SkipRateLimit = (skip = true) => SetMetadata(RATE_LIMIT_SKIP, skip);
